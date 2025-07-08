20a68e3194782ec07ad6aa7ef586ca8089b82229
ansari-dot <0349ansari@gmail.com>
2025-07-08 19:29:56 +0500

    mongo uri update

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/GuestHouse';
+        const uri = process.env.MONGO_URI || 'mongodb+srv://codewithansari:codewithansari@merrige.unitf.mongodb.net/?retryWrites=true&w=majority&appName=Merrige';
         const conn = await mongoose.connect(uri);
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
